a6612812744a480cd5de1b051258029173dfd8fe
khoaphamanh <pham.anhkhoa1215@gmail.com>
2026-04-26 14:35:12 +0200

    add pi

--- rl_exercises/week_2/policy_iteration.py
+++ rl_exercises/week_2/policy_iteration.py
@@ -59,8 +59,10 @@ class PolicyIteration(AbstractAgent):
 
         # Initialize policy and Q-values
         rng = np.random.default_rng(seed=self.seed)
-        self.pi: np.ndarray = rng.integers(0, self.n_actions, self.n_obs)
-        self.Q = np.zeros_like(self.R_sa)
+        self.pi: np.ndarray = rng.integers(
+            0, self.n_actions, self.n_obs
+        )  # (5, ) [1, 0, 1, 1, 0] at which state do what?
+        self.Q = np.zeros_like(self.R_sa)  # Q.shape = (5, 2)
 
         self.policy_fitted: bool = False
         self.steps: int = 0
@@ -85,18 +87,24 @@ class PolicyIteration(AbstractAgent):
         tuple[int, dict]
             The selected action and an empty info dictionary.
         """
-        # TODO: Return the action according to current policy
-        raise NotImplementedError("predict_action() is not implemented.")
+        action = int(self.pi[observation])
+        return action, {}
 
     def update_agent(self, *args: tuple, **kwargs: dict) -> None:
         """Run policy iteration to compute the optimal policy and state-action values."""
         if not self.policy_fitted:
-            # TODO: Call policy iteration with initialized values
             printr("Initial policy: ", self.pi)
-            raise NotImplementedError("update_agent() is not implemented.")
+
+            self.Q, self.pi, self.steps = policy_iteration(
+                self.Q,
+                self.pi,
+                (self.S, self.A, self.T, self.R_sa, self.gamma),
+            )
+
             printr("Q: ", self.Q)
             printr("Final policy: ", self.pi)
             printr("Policy iteration steps:", self.steps)
+
             self.policy_fitted = True
 
     def save(self, *args: tuple[Any], **kwargs: dict) -> None:
@@ -155,10 +163,25 @@ def policy_evaluation(
     np.ndarray
         The evaluated value function V[s] for all states.
     """
+    # R_sa = [
+    #     [1, 0],
+    #     [1, 0],
+    #     [0, 0],
+    #     [0, 10],
+    #     [0, 10],
+    # ]
     nS = R_sa.shape[0]
-    V = np.zeros(nS)
+    V = np.zeros(nS)  # shape (5, ) value function for each state
 
-    # TODO: implement Policy Evaluation for all states
+    while True:
+        V_prev = V.copy()
+        for s in range(nS):
+            a = pi[s]
+            V[s] = R_sa[s, a] + gamma * np.sum(T[s, a] * V_prev)
+
+        # convergenz check
+        if np.max(np.abs(V - V_prev)) < epsilon:
+            break
 
     return V
 
@@ -191,7 +214,11 @@ def policy_improvement(
     nS, nA = R_sa.shape
     Q = np.zeros((nS, nA))
     pi_new = None
-    # TODO: implement Policy Improvement for all states
+    for s in range(nS):
+        for a in range(nA):
+            Q[s, a] = R_sa[s, a] + gamma * np.sum(T[s, a, :] * V)
+
+    pi_new = np.argmax(Q, axis=1)
 
     return Q, pi_new
 
@@ -223,7 +250,21 @@ def policy_iteration(
     """
     S, A, T, R_sa, gamma = MDP
 
-    # TODO: Combine evaluation and improvement in a loop.
+    steps = 0
+
+    while True:
+        old_pi = pi.copy()
+
+        V = policy_evaluation(pi, T, R_sa, gamma, epsilon)
+        Q, pi = policy_improvement(V, T, R_sa, gamma)
+
+        steps += 1
+
+        # stop until policy converges (no change in policy
+        if np.array_equal(pi, old_pi):
+            break
+
+    return Q, pi, steps
 
 
 if __name__ == "__main__":
