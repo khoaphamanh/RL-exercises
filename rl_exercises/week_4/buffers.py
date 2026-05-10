1ebd24819a50490cf5ccdcadb56f178f3c51554f
khoaphamanh <pham.anhkhoa1215@gmail.com>
2026-05-10 14:53:57 +0200

    add bufffers

--- rl_exercises/week_4/buffers.py
+++ rl_exercises/week_4/buffers.py
@@ -57,12 +57,23 @@ class ReplayBuffer(AbstractBuffer):
         info : dict
             Gym info dict (can store extras).
         """
-        if len(self.states) >= self.capacity:
-            # TODO: pop the oldest element off each list (states, actions, …, infos)
-            # pop oldest
-            return
 
-        # TODO: append state, action, reward, next_state, done, info to their respective lists
+        # remove oldest transition if buffer is full laut first in first out (FIFO) policy
+        if len(self.states) >= self.capacity:
+            self.states.pop(0)
+            self.actions.pop(0)
+            self.rewards.pop(0)
+            self.next_states.pop(0)
+            self.dones.pop(0)
+            self.infos.pop(0)
+
+        # append state, action, reward, next_state, done, info to their respective lists
+        self.states.append(state)
+        self.actions.append(int(action))
+        self.rewards.append(float(reward))
+        self.next_states.append(next_state)
+        self.dones.append(bool(done))
+        self.infos.append(info)
 
     def sample(
         self, batch_size: int = 32
@@ -80,7 +91,9 @@ class ReplayBuffer(AbstractBuffer):
         List of transitions as (state, action, reward, next_state, done, info).
         """
         # TODO: randomly choose `batch_size` unique indices from [0, len(self.states))
-        idxs = ...
+        idxs = np.random.choice(
+            len(self.states), size=min(batch_size, len(self.states)), replace=False
+        )
         return [
             (
                 self.states[i],
@@ -96,3 +109,30 @@ class ReplayBuffer(AbstractBuffer):
     def __len__(self) -> int:
         """Current number of stored transitions."""
         return len(self.states)
+
+
+# note
+"""
+transition = (state, action, reward, next_state, done, info)
+capacity = max number of transitions to store
+
+#steps to steps
+take one action in environment
+get one new transition
+add it to replay buffer
+sample random batch of 32 from replay buffer
+train once
+repeat
+
+# example 
+step 1: buffer has 1 transition, no training yet
+step 2: buffer has 2 transitions, no training yet
+...
+step 32: buffer has 32 transitions, sample 32, train once
+step 33: buffer has 33 transitions, sample 32, train once
+step 34: buffer has 34 transitions, sample 32, train once
+...
+step 1000: buffer has 1000 transitions, sample 32, train once
+step 1001: remove oldest, add newest, buffer still has 1000, sample 32, train once
+
+"""
