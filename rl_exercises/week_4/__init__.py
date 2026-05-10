e5c094d5c92193e8ae6981a6b065b2bee8b93ff7
khoaphamanh <pham.anhkhoa1215@gmail.com>
2026-05-10 21:42:41 +0200

    init

--- rl_exercises/week_4/__init__.py
+++ rl_exercises/week_4/__init__.py
@@ -1,5 +1,5 @@
-from rl_exercises.week_4.buffers import ReplayBuffer
+from rl_exercises.week_4.buffers import PrioritizedReplayBuffer, ReplayBuffer
 from rl_exercises.week_4.dqn import DQNAgent
 from rl_exercises.week_4.networks import QNetwork
 
-__all__ = ["DQNAgent", "ReplayBuffer", "QNetwork"]
+__all__ = ["DQNAgent", "ReplayBuffer", "PrioritizedReplayBuffer", "QNetwork"]
